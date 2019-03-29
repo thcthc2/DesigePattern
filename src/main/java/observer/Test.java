4aa9ea2b2d684dae0c3753f0c6ac9218c53b5836
tianhaochen <tianhcmail@163.com>
2019-03-29 18:01:21 +0800

    添加新用户

--- src/main/java/observer/Test.java
+++ src/main/java/observer/Test.java
@@ -5,11 +5,13 @@ public class Test {
         Observer ob = new User("张三");
         Observer ob1 = new User("李四");
         Observer ob2 = new User("王五");
+        Observer ob3 = new User("赵六");
 
         Observerable observerable = new Server();
         observerable.addObserver(ob);
         observerable.addObserver(ob1);
         observerable.addObserver(ob2);
+        observerable.addObserver(ob3);
 
         ((Server) observerable).setMsg("java是世界上最好的语言");
         System.out.println("-------------------------");
